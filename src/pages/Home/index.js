bc123b7681491cc1c94a1f26e140908f7275601f
Igor Cezatte <igorcezatte13@gmail.com>
2019-10-07 21:24:23 -0300

    feature: home style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/0336ac5ffd1b586ef4ac01dd10a5d6e0.jpeg"
+          alt="Tênis"
+        />
+        <strong>Tênis bom e barato</strong>
+        <span>R$1299,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/0336ac5ffd1b586ef4ac01dd10a5d6e0.jpeg"
+          alt="Tênis"
+        />
+        <strong>Tênis bom e barato</strong>
+        <span>R$1299,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/0336ac5ffd1b586ef4ac01dd10a5d6e0.jpeg"
+          alt="Tênis"
+        />
+        <strong>Tênis bom e barato</strong>
+        <span>R$1299,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/0336ac5ffd1b586ef4ac01dd10a5d6e0.jpeg"
+          alt="Tênis"
+        />
+        <strong>Tênis bom e barato</strong>
+        <span>R$1299,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/0336ac5ffd1b586ef4ac01dd10a5d6e0.jpeg"
+          alt="Tênis"
+        />
+        <strong>Tênis bom e barato</strong>
+        <span>R$1299,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/0336ac5ffd1b586ef4ac01dd10a5d6e0.jpeg"
+          alt="Tênis"
+        />
+        <strong>Tênis bom e barato</strong>
+        <span>R$1299,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
